3b9a9b7ea50e7b5cc51e37a41fd728422fba72af
Gustavo Braun <gtvbraun@gmail.com>
2023-04-16 13:34:27 -0300

    agregando componentes y ruteo

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,12 +1,26 @@
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Contador from './Contador';
+import Home from './Home';
+import NotFound from './NotFound';
+import Formulario from './Formulario';
 
 function App() {
   return (
     <div className="App">
+            
+        <ul>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/contador">Contador</Link></li>
+          <li><Link to="/registro">Registro</Link></li>
+        </ul>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contador' element={<Contador />} />
+          <Route path='/registro' element={<Formulario />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       
-      <Contador />
-
     </div>
   );
 }
---
+++ new-app/src/Formulario.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Formulario(){
+    
+    return(
+        <h3>
+            Formulario
+        </h3>
+    )
+}
+
+export default Formulario;
